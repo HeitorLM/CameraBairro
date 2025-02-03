9560efae2aee886bbc5e3eea1bdd5100e0094537
HeitorLM <heitor_lm@hotmail.com>
2025-02-03 04:26:35 -0300

    feat: modulariza arquivo m3.ts

--- src/m3.ts
+++ src/m3.ts
@@ -2,22 +2,7 @@ const fs = require('fs');
 const m3u8stream = require('m3u8stream')
 
 import CameraService from './cameras';
-
-// Função para quebrar a URL em 2 (mainURl e authURL)
-function splitUrl(url: string): string[] {
-    const parts = url.split('/');
-    const mainURL = parts.slice(0, 3).join('/');
-    const authURL = parts.slice(3).join('/');
-    return [mainURL, authURL];
-}
-
-// Função para carregar a URL e retornar a última linha da resposta
-async function getM3u8Url(url: string): Promise<string> {
-    const response = await fetch(url);
-    const text = await response.text();
-    const lines = text.split('\n');
-    return lines[lines.length - 1];
-}
+import { splitUrl, getM3u8Url } from './utils';
 
 const start = async () => {
     // Pega info das cameras
